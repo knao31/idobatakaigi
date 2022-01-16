876c9fd61e47a6475267f0c52170b26282b9a29e
桂 直幸 <katsuranaoyuki@macn2.local>
2022-01-16 13:36:41 +0900

    setName by clicking button

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import SignIn from './SignIn';
 
 export default () => {
-  return <div>Hello, React!</div>;
+  const [name, setName] = useState('');
+  console.log({ name });
+
+  return <SignIn setName={setName} />;
 };
